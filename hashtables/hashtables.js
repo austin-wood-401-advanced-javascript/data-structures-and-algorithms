14ac88f96d4e453f693d53263675e771f99c15f9
Awood549 <Austin.wood1776@live.com>
2019-10-27 23:15:16 -0700

    hashtables datastructure

--- hashtables/hashtables.js
+++ hashtables/hashtables.js
@@ -1,21 +1,47 @@
 'use strict';
 
 class Node {
-  constructor(value, next = null) {
+  constructor(value) {
     this.value = value;
-    this.next = next;
   }
 }
 
-class Hashtable{
-
-  constructor(item){
-    this.item = item;
-    this.table = [];
+class HashTable {
+  constructor(size) {
+    this.size = size;
+    this.map = new Array(size);
   }
 
-  add(key,value){
-    
+  add(key, value) {
+    let hashValue = this.hash(key);
+    if(!this.map[hashValue]) {this.map[hashValue] = [];}
+    let resultObj = {};
+    resultObj[key] = value;
+    this.map[hashValue].push(resultObj);
   }
+  hash(key) {
+    return key.split('').reduce((acc, val) => {
+      return acc + val.charCodeAt(0)
+    }, 0) * 599 % this.size;
+  }
+  
+  get(key) {
+    let hashKey = this.hash(key);
+    for(let i = 0; i < this.map[hashKey].length; i++) {
+      if(this.map[hashKey][i][key]) {return this.map[hashKey][i][key];}
+    }
+    return null;
+  }
+
+  contains(key) {
+    let hashKey = this.hash(key);
+    for(let i = 0; i < this.map[hashKey].length; i++) {
+      if(this.map[hashKey][i][key]) {return true;}
+    }
+    return null;
+  }
+
 
 }
+
+module.exports = HashTable;
