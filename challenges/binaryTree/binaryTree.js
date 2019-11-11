4972fa8a7926f50dd41d15b8195724528eeb0d08
Awood549 <Austin.wood1776@live.com>
2019-11-10 22:18:50 -0800

    find the max

--- challenges/binaryTree/binaryTree.js
+++ challenges/binaryTree/binaryTree.js
@@ -1,35 +1,226 @@
 'use strict';
 
-const myTree = require('../../tree/tree.js');
-
-let binaryTree = myTree.BinaryTree;
-
-class bread extends binaryTree{
-  constructor(){
-    super();
+class Node{
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-
-  breadFirst(){
-    console.log('PING');
-    if(!this.root){return null;}
-
-    let _walk = function(node){
-      console.log(node.value);
-      if(node.left){_walk(node.left);}
-      if(node.right){_walk(node.right);}
-    };
-    
-    _walk(this.root);
-  }
-
 }
 
-let tree = new bread();
-tree.add(1);
-tree.add(2);
-tree.add(3);
-tree.add(4);
-tree.add(5);
-tree.add(6);
-console.log(tree);
-tree.breadFirst();
+class Queue{
+  constructor(){
+    this.front = null;
+    this.back = null;
+    this.queue = [];
+  }
+  enqueue(value){
+    this.queue.push(value);
+    this.back = value;
+    // console.log(this.back);
+  }
+  dequeue(){
+    let value = this.queue.shift();
+    this.front = this.queue[0];
+    return value;
+  }
+  peek(){
+    return this.queue[0];
+  }
+}
+
+class BinaryTree{
+  constructor(root=null){
+    this.root=root;
+  }
+
+  add(value){
+    if(!this.root){
+      this.root=new Node(value);
+    }else{
+
+      let inTree = new Queue;
+
+      inTree.enqueue(this.root);
+      let current;
+
+      while(inTree.peek()){
+        current = inTree.dequeue();
+
+        if(current.left===null){
+          current.left=new Node(value);
+          break;
+        }else{
+          inTree.enqueue(current.left);
+        };
+
+        if(current.right===null){
+          current.right=new Node(value);
+          break;
+        }else{
+          inTree.enqueue(current.right);
+        };
+      }
+    }
+  }
+
+  contains(value){
+    let container=false;
+
+    let _walk =(node)=> {
+      if(node.value===value) container=true;
+  
+      if(node.left)_walk(node.left);
+      if(node.right) _walk(node.right);
+    };
+  
+    _walk(this.root);
+    return container;
+  }
+
+  preorder(){
+    let results = [];
+    
+    let _walk =(node)=> {
+      results.push(node.value);
+
+      if(node.left)_walk(node.left);
+      if(node.right) _walk(node.right);
+    };
+
+    _walk(this.root);
+    return results;
+  }
+
+  inOrder(){
+    let results = [];
+
+    let _walk=(node)=>{
+      if(node.left)_walk(node.left);
+      results.push(node.value);
+
+      if(node.right)_walk(node.right);
+    }
+
+    _walk(this.root);
+    return results;
+  }
+
+  postOrder(){
+    let results = [];
+    
+    let _walk =(node)=> {
+      if(node.left)_walk(node.left);
+      if(node.right)_walk(node.right);
+      results.push(node.value);
+    };
+  
+    _walk(this.root);
+  
+    return results;
+  };
+
+  levelOrder(){
+    let results = [];
+    let nodeQueue=[];
+
+    nodeQueue.push(this.root);
+
+    while(nodeQueue.length){
+      let current=nodeQueue.shift();
+      results.push(current.value);
+      if(current.left)nodeQueue.push(current.left);
+      if(current.right)nodeQueue.push(current.right);
+    }
+    return results;
+  }
+  
+  breadthFirst(){
+    if(!this.root)return null;
+
+    let inTree = new Queue;
+    let outTree = new Queue;
+
+    inTree.enqueue(this.root);
+    let current;
+
+    while(inTree.peek()){
+      current = inTree.dequeue();
+
+      if (current.left)inTree.enqueue(current.left);
+      if(current.right)inTree.enqueue(current.right);
+
+      console.log(current.value);
+      outTree.enqueue(current);
+    }
+  }
+
+  findMaximumValue(){
+
+    if(!this.root)return null;
+
+    let max = 0;
+
+    let _walk =(node)=> {
+      if (node.value>max)max=node.value;
+      if(node.left)_walk(node.left);
+      if(node.right) _walk(node.right);
+    };
+
+    _walk(this.root);
+    
+    return max;
+  }
+}
+
+class BinarySearchTree{
+  constructor(root=null){
+    this.root=root;
+  }
+
+  add(value){
+    if(!this.root){
+      this.root=new Node(value);
+    }else{
+
+      let inTree = new Queue;
+
+      inTree.enqueue(this.root);
+      let current;
+
+      while(inTree.peek()){
+        current = inTree.dequeue();
+
+        if(value<current.value&&current.left===null){
+          current.left=new Node(value);
+          break;
+        }else{
+          inTree.enqueue(current.left);
+        };
+
+        if(value>current.value&&current.right===null){
+          current.right=new Node(value);
+          break;
+        }else{
+          inTree.enqueue(current.right);
+        };
+      }
+    }
+  }
+
+  contains(value){
+    let container=false;
+
+    let _walk =(node)=> {
+      if(node.value===value) container=true;
+  
+      if(node.left)_walk(node.left);
+      if(node.right) _walk(node.right);
+    };
+  
+    _walk(this.root);
+    return container;
+  }
+}
+
+module.exports=BinaryTree;
---
+++ challenges/binaryTree/readme.md
@@ -0,0 +1,21 @@
+# Challenge Summary
+Code Challenge DSA, Implement A Binary Tree and A Binary Search Tree.
+
+## Challenge Description
+Create a Node class that has properties for the value stored in the node, the left child node, and the right child node.
+
+Create a BinaryTree class:
+
+Define a method for each of the depth first traversals called preOrder, inOrder, and postOrder which returns an array of the values, ordered appropriately.
+At no time should an exception or stack trace be shown to the end user. Catch and handle any such exceptions and return a printed value or operation which cleanly represents the state and either stops execution cleanly, or provides the user with clear direction and output.
+
+Create a BinarySearchTree class:
+
+Define a method named add that accepts a value, and adds a new node with that value in the correct location in the binary search tree.
+Define a method named contains that accepts a value, and returns a boolean indicating whether or not the value is in the tree at least once.
+
+## Approach & Efficiency
+For the approach of this challenge, I created a Node class, and a Binary Tree class that had preOrder(), postOrder(), and inOrder() methods in it. Each of these methods traverse the Binary Tree in different ways. I then created a Binary Search Tree class that extends the Binary Tree class. This contains the add() and contains() methods, which add a new Node to a tree, and search to see if the Tree contains a specific value, respectively.
+
+## Solution
+![Code Challenge DSA: Tree Image](../)
