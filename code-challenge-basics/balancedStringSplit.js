d40aeb11ff847498a3a48b9b86449a8cc20ca7c2
Austin Wood <44730393+Awood549@users.noreply.github.com>
2020-04-18 20:51:51 -0700

    Create balancedStringSplit.js

---
+++ code-challenge-basics/balancedStringSplit.js
@@ -0,0 +1,16 @@
+var balancedStringSplit = function(s) {
+    let R = 0;
+    let L = 0;
+    let result= 0 
+    for(let i=0; i < s.length; i++){
+        if(s[i]== 'R'){
+            R++
+        } else{
+            L++
+        }
+        if(R === L){
+            result++
+        }
+    }
+    return result
+};
